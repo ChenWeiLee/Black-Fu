17677478bf3e62957e4469e3d62b53edd289f479
Li Chen wei <enoch_lee@hiiir.com>
2015-12-22 20:12:38 +0800

    [Code] 新增關鍵字搜尋

--- Black-Fu/Black-Fu/ViewController.swift
+++ Black-Fu/Black-Fu/ViewController.swift
@@ -12,10 +12,16 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     @IBOutlet var tableView: UITableView!
     
+    
+    var tableAry:[String] = ["林鳳營","統一布丁","林老師","大醇豆","36法郎","每日C-柳橙","每日C-葡萄"]
+    var tableShowAry:[String] = []
     //MARK: - ViewController 生命週期
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableShowAry = tableAry
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -35,6 +41,20 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
 
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         
+        tableShowAry.removeAll()
+        
+        if searchText == "" {
+            tableShowAry = tableAry
+        }else{
+            for tableString:String in tableAry {
+                if tableString.rangeOfString(searchText) != nil {
+                    tableShowAry.append(tableString)
+                }
+            }
+        }
+
+        tableView.reloadData()
+        
     }
     
     // MARK: - UITableView Delegate & DataSource
@@ -42,12 +62,12 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
-        return 0;
+        return tableShowAry.count;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
-        
+        cell.textLabel?.text = tableShowAry[indexPath.row]
         
         return cell
     }
