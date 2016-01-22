1e269fbd75c4c5223896211b5043655a16d10ff6
Li Chen wei <enoch_lee@hiiir.com>
2016-01-23 01:05:02 +0800

    [Code]Add a singleton

---
+++ Black-Fu/Black-Fu/singletonObject.swift
@@ -0,0 +1,13 @@
+//
+//  singletonObject.swift
+//  Black-Fu
+//
+//  Created by Li Chen wei on 2016/1/23.
+//  Copyright © 2016年 TWML. All rights reserved.
+//
+
+import UIKit
+
+class singletonObject: NSObject {
+
+}
